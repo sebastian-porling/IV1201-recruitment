4e7b4a79f157c87593b2aff6df4ba43f47b38111
sebastian-porling <sebastian@n142-p75.eduroam.kth.se>
2019-01-31 15:40:11 +0100

    Added JSDoc

--- server/integration/db.js
+++ server/integration/db.js
@@ -1,5 +1,9 @@
 const mongodb = require('mongodb');
 
+/**
+ * Loads the user collection
+ * @returns the collection
+ */
 exports.loadUsersCollection = async function loadUsersCollection() {
     const client = await mongodb.MongoClient.connect('mongodb://IV1201:IV1201@ds119993.mlab.com:19993/recruitment', {
         useNewUrlParser: true
