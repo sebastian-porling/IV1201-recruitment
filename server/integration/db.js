f07edade5f60d14e1d8b7586447a52cde014fe38
sebastian-porling <sebastian.porling@gmail.com>
2019-03-07 09:18:15 +0100

    Test with testdatabase

--- server/integration/db.js
+++ server/integration/db.js
@@ -72,14 +72,15 @@ process.on('SIGINT', () => {
 /* Mongo.js*/
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://IV1201:IV1201@recruitment-shard-00-00-gxbqo.mongodb.net:27017,recruitment-shard-00-01-gxbqo.mongodb.net:27017,recruitment-shard-00-02-gxbqo.mongodb.net:27017/test?ssl=true&replicaSet=recruitment-shard-0&authSource=admin&retryWrites=true'"; 
+var url = process.env.MONGOLAB_URI || "mongodb://IV1201:IV1201@recruitment-shard-00-00-gxbqo.mongodb.net:27017,recruitment-shard-00-01-gxbqo.mongodb.net:27017,recruitment-shard-00-02-gxbqo.mongodb.net:27017/test?ssl=true&replicaSet=recruitment-shard-0&authSource=admin&retryWrites=true'"; 
+console.log(url);
 var assert = require('assert');
 
 var connection=[];
 // Create the database connection
 establishConnection = function(callback){
   MongoClient.connect(url, {useNewUrlParser: true,
-    replicaSet: 'recruitment-shard-0' ,
+    replicaSet: process.env.REPLICA_SET || 'recruitment-shard-0' ,
     readConcern: { level: "majority" }, poolSize: 10 },function(err, db) {
     assert.equal(null, err);
       connection = db
